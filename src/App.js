69f398db1598168c9e3793c90b88f225b6b33cd7
phatnguyen6492 <phat.nguyen@incepit.com>
2022-03-20 10:19:42 +0700

    fix-unused-var

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { render } from 'react-dom';
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
